0fd1c4c1df225aaadcc0933148d9273a49060129
hirpara-jitendra <hirpara.jitendra@gmail.com>
2017-04-02 12:10:56 +0530

    Added Spring Config Server and spring profiles capabilities.

--- ipl/src/main/java/com/ipl/config/Application.java
+++ ipl/src/main/java/com/ipl/config/Application.java
@@ -3,16 +3,18 @@ package com.ipl.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com")
 @EntityScan(basePackages = "com")
+@EnableConfigServer
 public class Application {
  
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
     }
-    
 }
+    
    
