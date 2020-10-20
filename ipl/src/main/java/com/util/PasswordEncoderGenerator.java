94687be5952bbca31b2867fdb44de51a46e6cef2
Jitendra <hirpara.jitendra@gmail.com>
2020-10-20 18:09:48 +0530

    comment main method

--- ipl/src/main/java/com/util/PasswordEncoderGenerator.java
+++ ipl/src/main/java/com/util/PasswordEncoderGenerator.java
@@ -4,10 +4,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoderGenerator {
 
- /*  public static void main(String args[]) {
+ /*public static void main(String args[]) {
 
         //Generate passwords here before starting app to run user script otherwise login will not work
-        String password = "nicetrykeepitup";
+        String password = "";
        //String password = "password";
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(password);
