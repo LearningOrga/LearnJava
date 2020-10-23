80ff26eb35e2f77b269c9338139c10ebfb876c6e
Jitendra <hirpara.jitendra@gmail.com>
2020-10-23 10:18:55 +0530

    move to root

--- ipl/src/main/java/com/backend/ServletInitializer.java
+++ ipl/src/main/java/com/backend/ServletInitializer.java
@@ -1,12 +1,15 @@
-package com.backend.config;
+package com.backend;
 
+import com.backend.config.Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        System.out.print("HELLO");
         return application.sources(Application.class);
+
     }
 }
 
